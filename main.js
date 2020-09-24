8e6757372816768acab56a40d8c362d165db064f
javalo <lo.cheikhna@gmail.com>
2020-09-24 14:38:13 +0200

    gestion des catch with screen shot

--- main.js
+++ main.js
@@ -22,3 +22,30 @@ function loadTechnologies(technos) {
 }
  
 loadTechnologies(technos);
+
+	
+// ..
+if(navigator.serviceWorker) {
+    navigator.serviceWorker
+        .register('sw.js')
+        .catch(err => console.error('service worker NON enregistré', err));
+}
+/*
+if(window.caches) {
+    caches.open('veille-techno-1.0');
+    caches.open('other-1.0');
+    caches.keys().then(console.log);
+}
+
+*/
+
+//..
+if(window.caches) {
+    caches.open('veille-techno-1.0').then(cache => {
+        cache.addAll([
+            'index.html',
+            'main.js',
+            'vendors/bootstrap4.min.css'
+        ]);
+    });
+} 
