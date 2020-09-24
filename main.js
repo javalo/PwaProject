f13880085aff91dddbd93f11c7e8fa474a0f3a94
javalo <lo.cheikhna@gmail.com>
2020-09-24 13:54:00 +0200

    Sever connected to the app +  the first screenShot

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-console.log('hello depuis main');
 const technosDiv = document.querySelector('#technos');
-
+ 
 function loadTechnologies(technos) {
     fetch('http://localhost:3001/technos')
         .then(response => {
             response.json()
                 .then(technos => {
-                    const allTechnos = technos.map(t => `<div><b>${t.name}</b> ${t.description}  <a href="${t.url}">site de ${t.name}</a> </div>`)
+                    const allTechnos = technos.map(t => `
+                    <div class="card">
+                        <div class="card-body">
+                        <h5 class="card-title">${t.name}</h5>
+                        <p class="card-text">${t.description}</p>
+                        <a href="${t.url}" class="card-link">site de ${t.name}</a>
+                        </div>
+                    </div>`)
                             .join('');
             
                     technosDiv.innerHTML = allTechnos; 
@@ -14,5 +20,5 @@ function loadTechnologies(technos) {
         })
         .catch(console.error);
 }
-
+ 
 loadTechnologies(technos);
